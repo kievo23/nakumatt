fa9ddd78030e9c4d7f870b129ffcd9e84015946d
kievo23 <kelvinchege@gmail.com>
2018-03-17 23:36:05 +0300

    initial commit

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+var sys = require(__dirname + '/../config/System');
+var db = mongoose.connect(sys.db_uri, {useMongoClient: true });
+mongoose.Promise =require('bluebird');
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+	amount: String,
+	phone: String,
+	date: Date,
+	trancode: String,
+	processed: Boolean,
+	delivery: String
+});
+
+module.exports = mongoose.model('Order', orderSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var Order = require(__dirname + '/../models/Product');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/receive', function(req, res, next){
-	console.log("params console");
-	console.log(req.params);
-
-	console.log("Query console");
-	console.log(req.query);
 
 	console.log("body console");
 	console.log(req.body);
+
+	Order.create({
+		amount: req.body.stkCallback.CallbackMetadata.Item[0].Value,
+		phone: req.body.stkCallback.CallbackMetadata.Item[4].Value,
+		date: new Date(),
+		transcode: req.body.stkCallback.CheckoutRequestID
+	}, function(err, rst){
+		if(err)
+			console.log(err);
+		console.log(rst);
+	});
 });
 
 module.exports = router;
